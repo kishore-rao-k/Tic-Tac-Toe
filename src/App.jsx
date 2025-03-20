2ef86fd7bba6857c554762bdc3312ef78ac02cf7
kishore-rao-k <kishoreraok12@gmail.com>
2025-03-20 14:04:10 +0530

    updated timeTravel

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,51 @@ function Square({value,onSquareClick}){
   return( <button className="square" onClick={onSquareClick}>{value}</button>)
 }
 
-function App() {
-  const [xIsNext,setXIsNext] = useState(true);
-  const [squares,setSquares] = useState(Array(9).fill(null));
+ function Game(){
+
+  const [history,setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+  
+  function handlePlay(nextSquares){
+    const nextHistory = [...history.slice(0,currentMove+1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length-1);
+   
+  }
+  function jumpTo(nextMove){
+    setCurrentMove(nextMove);
+    
+  }
+  const moves = history.map((squares,move)=>{
+    let description;
+    if(move > 0){
+      description = 'Go to move #' + move;
+    }else{
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={()=> jumpTo(move)}> {description}</button>
+      </li>
+    )
+  })
+  return(
+    <div className='game'>
+      <div className='game-board'>
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay}/>
+      </div>
+      <div className='game-info'>
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  )
+}
+
+
+function Board({xIsNext,squares,onPlay}) {
+
   function handleClick(i){
     if(squares[i] || calculateWinner(squares)){
       return;
@@ -18,8 +60,8 @@ function App() {
     }else{
       nextSquares[i] = "O";
     }
-    setSquares(nextSquares);
-    setXIsNext(!xIsNext)
+    onPlay(nextSquares);
+    
   }
   
   const winner = calculateWinner(squares);
@@ -54,6 +96,7 @@ function App() {
 }
 
 
+
 function calculateWinner(squares) {
   const lines = [
     [0, 1, 2],
@@ -74,7 +117,7 @@ function calculateWinner(squares) {
   return null;
 }
 
-export default App
+export default Game
 
 
 
